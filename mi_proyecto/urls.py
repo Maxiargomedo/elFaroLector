16d2dc52a0a5109cbcfbc1d83091275b7d801cbd
Maxiargomedo <max.argomedo@duocuc.cl>
2026-06-20 21:21:29 -0400

    Configure production media serve and bundle final improvements

--- mi_proyecto/urls.py
+++ mi_proyecto/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve as media_serve
 from django.http import HttpResponse
 
 def ping_view(request):
@@ -31,4 +31,6 @@ urlpatterns = [
     path('ping/', ping_view, name='ping'),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', media_serve, {'document_root': settings.MEDIA_ROOT}),
+]
