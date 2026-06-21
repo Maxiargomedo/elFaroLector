563b0e0e1e968764c2bad4c76a94edfd0ee4f115
Maxiargomedo <max.argomedo@duocuc.cl>
2026-06-20 20:41:30 -0400

    Serve media files in production

--- mi_proyecto/urls.py
+++ mi_proyecto/urls.py
@@ -31,5 +31,4 @@ urlpatterns = [
     path('ping/', ping_view, name='ping'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
